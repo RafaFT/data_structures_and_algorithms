f3242349b17f0d5541e3c3adcaad81db2c76ad8e
RafaFT <rafael.ftrad@gmail.com>
2024-03-18 23:12:27 -0300

    feat: add right bisect implementation.

--- bisect/search.go
+++ bisect/search.go
@@ -55,3 +55,24 @@ func BisectLeft[T cmp.Ordered](s []T, value T) int {
 
 	return left
 }
+
+// BisectRight returns the highest index of s where v should be placed to keep order.
+// Index might equal len(s).
+//
+// s MUST be sorted in ascending order.
+//
+// Time O(log(n)) and space O(1).
+func BisectRight[T cmp.Ordered](s []T, v T) int {
+	left, right := 0, len(s)-1
+
+	for left <= right {
+		m := ((right - left) / 2) + left
+		if value := s[m]; v >= value {
+			left = m + 1
+		} else {
+			right = m - 1
+		}
+	}
+
+	return left
+}
--- bisect/search_test.go
+++ bisect/search_test.go
@@ -131,3 +131,78 @@ func TestBisectLeft(t *testing.T) {
 		}
 	}
 }
+
+func TestBisectRight(t *testing.T) {
+	tests := []struct {
+		s    []string
+		v    string
+		want int
+	}{
+		{
+			[]string{},
+			"a",
+			0,
+		},
+		{
+			[]string{"b"},
+			"a",
+			0,
+		},
+		{
+			[]string{"b"},
+			"b",
+			1,
+		},
+		{
+			[]string{"b"},
+			"c",
+			1,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"a",
+			0,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"b",
+			1,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"c",
+			1,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"d",
+			2,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"e",
+			2,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"f",
+			3,
+		},
+		{
+			[]string{"b", "d", "f"},
+			"g",
+			3,
+		},
+		{
+			[]string{"b", "b", "b"},
+			"b",
+			3,
+		},
+	}
+
+	for i, test := range tests {
+		if got := BisectRight(test.s, test.v); got != test.want {
+			t.Errorf("%d: BisectRight(%v, %v) = %d, want %d", i, test.s, test.v, got, test.want)
+		}
+	}
+}
