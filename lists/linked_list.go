e243d2f22937d154c4c911b5229233805127f352
RafaFT <rafael.ftrad@gmail.com>
2024-08-04 11:44:12 -0300

    feat: add initial linked list implementation.

---
+++ lists/linked_list.go
@@ -0,0 +1,103 @@
+package lists
+
+import "fmt"
+
+type LinkedList[T comparable] struct {
+	len  int
+	head *node[T]
+}
+
+type node[T comparable] struct {
+	value T
+	next  *node[T]
+}
+
+// Read returns the value at the provided index.
+// It panic if index is out of bounds.
+//
+// Time O(n) and space O(1).
+func (l *LinkedList[T]) Read(index int) T {
+	if index < 0 || index >= l.len {
+		panic(fmt.Sprintf("index out of range [%d] with length %d", index, l.len))
+	}
+
+	currentNode := l.head
+	for i := 1; i <= index; i++ {
+		currentNode = currentNode.next
+	}
+
+	return currentNode.value
+}
+
+// Search returns the first index that contains value or -1.
+//
+// Time O(n) and space O(1).
+func (l *LinkedList[T]) Search(value T) int {
+	i := 0
+	for currentNode := l.head; currentNode != nil; currentNode = currentNode.next {
+		if currentNode.value == value {
+			return i
+		}
+		i++
+	}
+
+	return -1
+}
+
+// Insert inserts value at the provided index.
+// It panics if index is out of range.
+//
+// Time O(n) and space O(1).
+func (l *LinkedList[T]) Insert(value T, index int) {
+	if index < 0 || index > l.len {
+		panic(fmt.Sprintf("index out of range [%d] with length %d", index, l.len))
+	}
+
+	l.len++
+	newNode := &node[T]{
+		value: value,
+		next:  nil,
+	}
+
+	if index == 0 {
+		newNode.next = l.head
+		l.head = newNode
+		return
+	}
+
+	prevNode := l.head
+	for i := 1; i < index; i++ {
+		prevNode = prevNode.next
+	}
+
+	newNode.next = prevNode.next
+	prevNode.next = newNode
+}
+
+// Delete removes value at provided index.
+// It panics if index is out of bounds.
+//
+// Time O(n) and space O(1).
+func (l *LinkedList[T]) Delete(index int) {
+	if index < 0 || index >= l.len {
+		panic(fmt.Sprintf("index out of range [%d] with length %d", index, l.len))
+	}
+
+	l.len--
+	if l.len == 0 {
+		l.head = nil
+		return
+	}
+
+	if index == 0 {
+		l.head = l.head.next
+		return
+	}
+
+	prevNode := l.head
+	for i := 1; i < index; i++ {
+		prevNode = prevNode.next
+	}
+
+	prevNode.next = prevNode.next.next
+}
